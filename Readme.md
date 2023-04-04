1a118302c56af8dcdad8868d7051186368133d00
Flexible Games <simtropbuggi@gmail.com>
2023-04-04 13:25:48 -0500

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Flexible Tools
+A mod for Vintage Story https://mods.vintagestory.at/show/mod/3103
+
+## Details
+Adds some very helpful if very expensive tools to the game. Each tool tackles a different aspect of the game to help players.
+- Diamond Scythe - Offers THE best harvest areas. Clear a 3x3 to 9x9 area of grass or your farm. Use "F" to set modes.
+- Diamond Shears - Clears leaves in a 3x3x3 to 9x9x9 area.
+- Item Magnet - A necklace-like Torc you place on your character. This was a fun (and challenging) item to program and is SUPER helpful. When worn, it pulls in items around you in a 4 block radius.
+- Rock Sniffer - A QoL item that only reports unique rock types below the rock block broken. May also report oil, if you have that mod installed. It does not give depth information.
+- Diamond Tipped Steel Pickaxe - Is just that. A tough pickaxe and primarily used to craft:
+- World Eater - A true BEAST of a tool. Breaks everything with ease (think paxel). Offers 3x3 and 5x5 clearing options as well as **VEIN MINING** of ores, clay, sand, gravel, snow, ice, and peat. This will fill your inventory very fast.
+
+## Performance
+To prevent your FPS from crashing when using the world eater the tool attempts to place block drops directly into the players inventory. It will only drop items on the ground if the player is full. Before you vein mine something, ensure you have plenty of inventory space available.
+
+## Code
+This is available for learning purposes. The issue tracker is available for bugs, and feel free to do a pull request for features or fixes. Keep them specific please.
+
+## Support?
+If you want to support me on any of my projects, please consider being a patron.
+It can take many many hours to finish a mod, keep it updated, and fix any bugs found by users. Please consider supporting the authors of your favorite mods.
+https://www.patreon.com/flexiblegames
---
+++ code/FlexibleTools/FlexibleTools/RockSnifferItem.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Vintagestory.API.Common;
+using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+
+namespace FlexibleTools
+{
+    class RockSnifferItem : Item
+    {        
+        private ICoreClientAPI capi;
+        private ICoreServerAPI sapi;
+        private bool isRock = false;
+
+        public override float OnBlockBreaking(IPlayer player, BlockSelection blockSel, ItemSlot itemslot, float remainingResistance, float dt, int counter)
+        {
+            Block block = player.Entity.World.BlockAccessor.GetBlock(blockSel.Position);
+            // normal rock in game is rock-{type}
+            if (block.FirstCodePart() == "rock")
+            {
+                isRock = true;
+            }
+            else isRock = false;
+
+            return base.OnBlockBreaking(player, blockSel, itemslot, remainingResistance, dt, counter);
+        }
+
+        public override bool OnBlockBrokenWith(IWorldAccessor world, Entity byEntity, ItemSlot itemslot, BlockSelection blockSel, float dropQuantityMultiplier = 1)
+        {
+            if (!isRock)
+            {
+                return base.OnBlockBrokenWith(world, byEntity, itemslot, blockSel, dropQuantityMultiplier);
+            }
+            // we're hitting rock! Time to Probe the Ground...
+            if (byEntity is EntityPlayer)
+            {
+                IPlayer player = world.PlayerByUid((byEntity as EntityPlayer).PlayerUID);
+                if (!player.Entity.World.Claims.TryAccess(player, blockSel.Position, EnumBlockAccessFlags.BuildOrBreak)) // honor land claims
+                {
+                    itemslot.MarkDirty();
+                    return false;
+                }
+                List<string> rockTypes = new List<string>();
+                rockTypes = RockSniff(player, world, blockSel);
+                if (rockTypes.Count == 0)
+                {
+                    if (api.Side == EnumAppSide.Client) capi.ShowChatMessage("Oddly, There are no rocks under you...");
+                    return true;
+                }
+                if (api.Side == EnumAppSide.Client) capi.ShowChatMessage("Rock Found:");
+                foreach (string rtype in rockTypes)
+                {
+                    if (api.Side == EnumAppSide.Client)
+                    {
+                        capi.ShowChatMessage(rtype);
+                    }
+                }
+            }
+            return base.OnBlockBrokenWith(world, byEntity, itemslot, blockSel, dropQuantityMultiplier); 
+        }
+
+        private List<string> RockSniff(IPlayer player, IWorldAccessor world, BlockSelection blockSel)
+        {
+            if (player == null) return null;
+            if (world == null) return null;
+            if (blockSel == null) return null;
+
+            List<string> rockTypes = new List<string>();
+            Block curBlock;
+            BlockPos blockpos = new BlockPos(blockSel.Position.X, blockSel.Position.Y, blockSel.Position.Z);
+
+            for (int offset = 1; offset < blockSel.Position.Y; offset++)
+            {
+                curBlock = world.BlockAccessor.GetBlock(blockpos.X, blockpos.Y - offset, blockpos.Z);
+                if (curBlock.FirstCodePart() == "rock" || curBlock.FirstCodePart() == "lava" || curBlock.FirstCodePart() == "oil")
+                {
+                    string codePart = curBlock.FirstCodePart() == "rock" ? curBlock.LastCodePart() : curBlock.FirstCodePart();
+
+                    if (!rockTypes.Contains(codePart))
+                    {
+                        rockTypes.Add(codePart);
+                    }
+                }
+            }
+            return rockTypes;
+        }
+
+
+        public override void OnLoaded(ICoreAPI api)
+        {
+            base.OnLoaded(api);
+            this.api = api;
+            if (api.Side == EnumAppSide.Server)
+            {
+                sapi = api as ICoreServerAPI;
+            }
+            else
+            {
+                capi = api as ICoreClientAPI;
+            }
+        }
+    }
+}
